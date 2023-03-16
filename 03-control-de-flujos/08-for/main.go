436a6e58cc36a5193ee701f6a6786de6fe621f52
servando <servandolopez20@gmail.com>
2023-03-16 17:39:32 -0600

    For loop

---
+++ 03-control-de-flujos/08-for/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*/Bucle infinito
+	for {
+		fmt.Println("Hola mundo")
+	}*/
+	/*/Bucle tipo while
+	numeros := 12455458
+	c := 0
+	for numeros > 0 {
+		numeros /= 10
+		c++
+
+	}
+	fmt.Println("Cantidad de digitos es: ", c)
+	*/
+	//Bucle for
+	for i := 0; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Print(i)
+		}
+	}
+}
