216b5e9105b9bc875052de080b6b21a874c267f7
Servando_Lopez <servandolopez20@gmail.com>
2023-03-21 12:33:05 -0600

    Continue and break

---
+++ 03-control-de-flujos/09-continue-break/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i <= 10; i++ {
+
+		if i == 5 {
+			fmt.Println("Salta la iteracion")
+			continue
+
+		}
+
+		if i == 8 {
+			fmt.Println("Romper con bucle ")
+			break
+
+		}
+		fmt.Println(i)
+	}
+
+}
