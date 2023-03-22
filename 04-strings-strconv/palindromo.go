a8a8e5361c41b449023c9bbe6b56e62017d7e6fd
Servando_Lopez <servandolopez20@gmail.com>
2023-03-22 17:06:37 -0600

    Funcion detectar palindromo

--- 04-strings-strconv/palindromo.go
+++ 04-strings-strconv/palindromo.go
@@ -5,15 +5,40 @@ import (
 	"strings"
 )
 
-func esPalindromo(palabra string) {
+func reverse(cadena string) string {
+
+	arrayCadena := strings.Split(cadena, "")
+	arrayInvertida := make([]string, 0)
+
+	for i := len(arrayCadena) - 1; i >= 0; i-- {
+		arrayInvertida = append(arrayInvertida, arrayCadena[i])
+	}
+	fmt.Println(arrayCadena)
+	fmt.Println(arrayInvertida)
+	return strings.Join(arrayInvertida, "")
+}
+
+func esPalindromo(palabra string) bool {
 	//Oso
 	fmt.Println(palabra)
 	palabra = strings.ToLower(palabra)
 	palabra = strings.ReplaceAll(palabra, " ", "")
 	fmt.Println(palabra)
+
+	palabraInvertida := reverse(palabra)
+
+	return palabra == palabraInvertida
 }
 
 func main() {
-	esPalindromo("Luz azul")
+
+	if esPalindromo("Luz azuli") {
+		fmt.Println("Es palindromo")
+	} else {
+		fmt.Println("No es palindromo")
+	}
+	//esPalindromo("Juan")
+
+	//reverse("Luz azul")
 
 }
