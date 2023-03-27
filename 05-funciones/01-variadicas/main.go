9ffcc26e8417463ec828f3429336941ff2a1342e
Servando_Lopez <servandolopez20@gmail.com>
2023-03-27 16:07:37 -0600

    Funciones variadicas

---
+++ 05-funciones/01-variadicas/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func sumar(nombre string, numeros ...int) (string, int) {
+
+	mensaje := fmt.Sprintf("La suma de %s es: ", nombre)
+	var total int
+	for _, num := range numeros {
+		total += num
+	}
+
+	return mensaje, total
+
+}
+
+func main() {
+	mensaje, result := sumar("Juan", 4, 5, 8, 6)
+	fmt.Println(mensaje, result)
+
+}
